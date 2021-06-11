9bbdf01b247b0e8dc81ecf3521fdecb5386b5ece
Sharaar <walidsharar@gmail.com>
2021-06-11 16:07:50 +0200

    Check the available resource and ask the user for the type of coffee

--- main.py
+++ main.py
@@ -25,8 +25,39 @@ MENU = {
 }
 
 profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+#todo check sufficient resources
+
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print("Sorry we are is not enough water.")
+            return False
+    return True
+#todo ask the user choice
+is_on=True
+while is_on:
+    choice = input("What would you like? espresso/latte/cuppocino  ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']} ml")
+        print(f"Milk: {resources['milk']} ml")
+        print(f"Coffee: {resources['coffee']} g")
+        print(f"Money: $ {profit}")
+# todo check the available resources
+    else:
+        drink =MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+
+
+
+
+
+
+    print(choice)
